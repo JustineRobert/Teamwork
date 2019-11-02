14d2e9d2098262b0f653faf0eef04b30c70a65a0
Justine Robert Igune <igunejustinerobert@gmail.com>
2019-11-02 16:41:28 +0300

    Added more changes in nodeauthapi

--- nodeauthapi/index.js
+++ nodeauthapi/index.js
@@ -10,12 +10,12 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.post('/api/posts', (req, res) =>{
+app.post('/api/posts', verifyToken, (req, res) =>{
   res.json({
     message: 'Post Created Successfully'
   });
 });
-
+/*
 app.post('/api/login', (req, res) => {
   //Mock user
   const user{
@@ -27,11 +27,31 @@ app.post('/api/login', (req, res) => {
     jobRole: 'Admin',
     department: 'Software Engineer',
     address: 'Kampala, Uganda'
-  }
-  jwt.sign({user:user}, 'secretkey', (err, token) =>{
+  } 
+
+  jwt.sign({user}, 'secretkey', (err, token) =>{
     res.json({
       token
     });
   });
-});
+}); */
+
+//TOKEN FORMAT
+//Authorization: Bearer <access_token>
+
+
+//Verify Token
+function verifyToken(req, res, next) {
+  //get auth header value
+  const bearerHeader = req.headers['authorization'];
+  //Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+
+  }else {
+    //Forbidden
+    res.sendStatus(403);
+  }
+}
+
+
 app.listen(5000, ()=>console.log('Server is running on port 5000'));
