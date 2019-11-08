a0e818af176df5927476be4373cd913c4a3e5cb6
Justine Robert Igune <igunejustinerobert@gmail.com>
2019-11-08 21:26:52 +0300

    Set updated on the servers

---
+++ servers/models/index.js
@@ -0,0 +1,8 @@
+import User from './user';
+import Article from './article';
+import Comment from './comment';
+import Category from './category';
+
+export {
+  User, Article, Comment, Category,
+};
