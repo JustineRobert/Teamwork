53b00f8d746ecc3e2f43fd3001b5328efa10790b
Igune Justine Robert <igunejustinerobert@gmail.com>
2019-11-10 20:46:23 +0300

    Modified file

---
+++ UI/js/scripts.js
@@ -0,0 +1,15 @@
+const deleteArticle = (slug, title) => {
+  confirm("Are you sure, you want to delete this article?\n"+title);
+};
+
+const userLogin = () => {
+window.location = "../pages/articles.html";
+};
+
+const userRegister = () => {
+  window.location = "../pages/articles.html";
+};
+
+const readArticle = () => {
+window.location = "../pages/readArticle.html";
+};
